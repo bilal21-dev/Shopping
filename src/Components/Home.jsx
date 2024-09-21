416ab545719513be9680479eef619cb598695b76
bilal21-dev <bilalahmar245@gmail.com>
2024-09-21 17:36:32 +0500

    hi

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,6 +5,7 @@ import Products from './Products';
 const Home = () => {
   return (
     <div className='bg-white'>
+      {/* jsodsa */}
       <div className='flex align-middle justify-center items-center gap-1 h-[42px]'>
         <input placeholder='Search Product' className='border border-black px-4 py-2 w-[700px] rounded-xl outline-0 mt-5' />
         <div className='bg-orange-600 h-full flex align-middle items-center justify-center w-[45px] rounded-xl text-white mt-5 '>
